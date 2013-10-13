3f55f48d01a3ff7b680ae6aabbc2e9d26dc93307
Stephen Cannavale <mybluecorners@gmail.com>
2013-10-13 20:34:42 +0000

    main.py edited online with Bitbucket

--- Code/main.py
+++ Code/main.py
@@ -18,8 +18,9 @@ class World(DirectObject):
         self.saucer = Saucer()
         base.disableMouse()
         camera.setPosHpr(0, -40, 55, 0, -15, 0)
-        self.loadModels()
         self.loadHUD()
+        self.loadModels()
+
         
         self.setupLights()
         self.keyMap = {"left":0, "right":0,"w":0,"a":0,"s":0,"d":0}
@@ -53,7 +54,6 @@ class World(DirectObject):
         taskMgr.add(self.textTask, "textTask")
         self.seconds = 0
         self.minutes = 0
-        
         self.animalsleft = 0
 
 
